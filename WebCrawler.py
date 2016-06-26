8515b6ee145c500707befdcced06f90678644ede
Pengfei Wu <wupengfei.wpf@gmail.com>
2016-06-25 22:36:58 -0700

    Only crawl html file

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,11 +1,12 @@
 import urlparse
-import LinkFinder
+from LinkFinder import LinkFinder
 import urllib
 from general import *
 
 __author__ = 'Pengfei Wu'
 
 MAX_CRAWLED_PAGE_NUM = 10
+WORK_DIR = 'build'
 
 class WebCrawler:
     base_url = ''
@@ -16,11 +17,14 @@ class WebCrawler:
     crawled_file = 'crawled.csv'
 
     def __init__(self, project_name, base_url):
+        # Basic info
         WebCrawler.base_url = base_url
         WebCrawler.project_name = project_name
-        WebCrawler.queue_file = os.path.join(project_name, WebCrawler.queue_file)
-        WebCrawler.crawled_file = os.path.join(project_name, WebCrawler.crawled_file) 
-        createWorkDir(WebCrawler.project_name)
+        # Setup directory and file
+        WebCrawler.queue_file = os.path.join(WORK_DIR, os.path.join(project_name, WebCrawler.queue_file))
+        WebCrawler.crawled_file = os.path.join(WORK_DIR, os.path.join(project_name, WebCrawler.crawled_file))
+        createWorkDir(os.path.join(WORK_DIR, WebCrawler.project_name))
+        # Init queue 
         WebCrawler.queue = readFile(WebCrawler.queue_file)
         WebCrawler.crawled = readFile(WebCrawler.crawled_file)
         WebCrawler.queue[WebCrawler.base_url] = (WebCrawler.project_name, 'This is start point')
@@ -28,6 +32,12 @@ class WebCrawler:
 
     # Maintain queue and crawled dict
     # get result from crawl_page and merge it to queue
+    # input resource: queue, crawled
+    # while queue not empty:
+    #       crawl one page
+    #       update queue and crawl
+    #       update queue and crawl file
+    # end
     @staticmethod
     def run():
         while len(WebCrawler.crawled) < MAX_CRAWLED_PAGE_NUM and len(WebCrawler.queue) > 0:
@@ -38,18 +48,27 @@ class WebCrawler:
                     continue
                 if n_url in WebCrawler.crawled:
                     continue
-                WebCrawler.queue[n_url] = value
+                WebCrawler.queue[n_url] = n_value
             WebCrawler.crawled[target_url] = target_value
             # write to file 
             appendFile(WebCrawler.crawled_file, target_url, target_value) 
         writeFile(WebCrawler.queue_file, WebCrawler.queue)
 
     # Crawled one page and return result
+    # input: target_url
+    # output: dictionary of link and data
+    # requirement: link are completed 
     @staticmethod
     def crawl_page(target_url):
         page_string = urllib.urlopen(target_url).read()
         link_finder = LinkFinder()
         link_finder.feed(page_string)
-        found_links = link_finder.get_found_links()
-        return res
+        raw_links = link_finder.get_found_links()
+        completed_links = {}
+        for k, v in raw_links.iteritems():
+            comp_link = urlparse.urljoin(WebCrawler.base_url, k)
+            if comp_link.lower().endswith('.html'):
+                completed_links[comp_link] = v
+        return completed_links
+
 
--- general.py
+++ general.py
@@ -23,17 +23,18 @@ def writeFile(file_name, urlRecs):
     with open(file_name, 'wb') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         for k, v in urlRecs.iteritems():
-            csvwriter.writerow([k, v])
+            csvwriter.writerow([k, v[0], v[1]])
 
 def readFile(file_name):
     res = dict()
     with open(file_name, 'rb') as csvfile:
         filereader = csv.reader(csvfile, delimiter=',')
         for row in filereader:
-             res[row[0]] = (row[1]. row[2])
+            print(row)
+            res[row[0]] = (row[1], row[2])
     return res
 
 def appendFile(file_name, i1, i2):
     with open(file_name, 'a') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerrow([i1, i2])
+        csvwriter.writerow([i1, i2[0], i2[1]])
